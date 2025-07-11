936331e89cea79bdeb3ac844e79ef5d02576efe1
anudeep <anudeep28.2000@gmail.com>
2025-07-11 12:42:07 +0530

    added credit to python morsels

--- tutorial/excercise03.py
+++ tutorial/excercise03.py
@@ -15,5 +15,7 @@ def add(matrix_1: List[List[int]], matrix_2: List[List[int]]) -> List[int]:
     >>> matrix2 = [[1, 1, 0], [1, -2, 3], [-2, 2, -2]]
     >>> add(matrix1, matrix2)
     [[2, -1, 3], [-3, 3, -3], [5, -6, 7]]
+
+    excercise taken from https://www.pythonmorsels.com/
     """
     raise NotImplementedError("Your implementation here")
